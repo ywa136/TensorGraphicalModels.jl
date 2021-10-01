abaf737a6bb89b12317bd318095efe25f3c9c819
Wayne Wang <wayneyw@umich.edu>
2021-10-01 15:19:28 -0400

    exported functions

--- src/TensorGraphicalModels.jl
+++ src/TensorGraphicalModels.jl
@@ -1,5 +1,21 @@
 module TensorGraphicalModels
 
-# Write your package code here.
+include("sg_palm.jl")
+include("kglasso.jl")
+include("teralasso.jl")
+include("glasso.jl")
+include("kron_pca.jl")
+include("utils.jl")
+include("utils_sim.jl")
+include("utils_teralasso.jl")
+include("enkf.jl")
+include("run_enkf.jl")
+
+export syglasso_palm
+export kglasso
+export teralasso
+export kron_pca
+export robust_kron_pca
+export glasso
 
 end
